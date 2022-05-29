0f77a1f9110ef07cf8e30af1a962df904910162c
programmer-jafir <jafiranjum2020@gmail.com>
2022-05-29 14:15:35 +0600

    limit Dashboard features based on admin level access

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
+import auth from '../../firebase.init';
+import useAdmin from '../../hooks/useAdmin';
 
 const Dashboard = () => {
+    const [user] = useAuthState(auth);
+    const [admin] = useAdmin(user)
     return (
         <div class="drawer drawer-mobile">
             <input id="dashboard-sidebar" type="checkbox" class="drawer-toggle" />
@@ -17,7 +22,7 @@ const Dashboard = () => {
                     {/* <!-- Sidebar content here --> */}
                     <li><Link to="/dashboard">My Orders</Link></li>
                     <li><Link to="/dashboard/review">My Reviews</Link></li>
-                    <li><Link to="/dashboard/Makeadmin">Make Admin</Link></li>
+                    { admin && <li><Link to="/dashboard/Makeadmin">Make Admin</Link></li>}
                 </ul>
 
             </div>
