8b34611dcd73f5bf07e133fa0ca38a09bfa8ed5c
programmer-jafir <jafiranjum2020@gmail.com>
2022-05-30 18:28:41 +0600

    admin doesn't see some pages

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -20,8 +20,8 @@ const Dashboard = () => {
                 <label for="dashboard-sidebar" class="drawer-overlay"></label>
                 <ul class="menu p-4 overflow-y-auto w-48 bg-base-100 text-base-content">
                     {/* <!-- Sidebar content here --> */}
-                    <li><Link to="/dashboard">My Orders</Link></li>
-                    <li><Link to="/dashboard/review">My Reviews</Link></li>
+                    { !admin &&<li><Link to="/dashboard">My Orders</Link></li>}
+                    { !admin && <li><Link to="/dashboard/review">My Reviews</Link></li>}
                     { admin && <li><Link to="/dashboard/Makeadmin">Make Admin</Link></li>}
                     { admin && <li><Link to="/dashboard/Addproduct">Add A Product</Link></li>}
                     { admin && <li><Link to="/dashboard/ManageProducts">Manage Products</Link></li>}
